f3bf55a1cc3ab8a10c4677d995471e4c9d317438
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-24 02:46:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,4 @@
 
 const nestedTarget = () => document.querySelector('#nested .target');
 const deepestChild = () => return document.querySelector('#grand-node div div div div');
-
 const getFirstSelector = (selector) => document.querySelector(selector); 
