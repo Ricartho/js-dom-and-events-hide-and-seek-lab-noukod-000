9bd71c65767173a97ccbf86c7acbe18daa44ee22
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-24 02:54:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const nestedTarget = () => document.querySelector('#nested .target');
 const deepestChild = () => document.querySelector('#grand-node div div div div');
 const getFirstSelector = (selector) => document.querySelector(selector); 
 const increaseRankBy = (n) => {
-  return 
+    const rankedLists = document.querySelectorAll('.ranked-list');
 }
